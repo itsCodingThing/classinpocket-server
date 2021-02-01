f86a7c7244845d6edbc32831c8f630484f708299
Bhanu Pratap Singh <bhanuparitapsingh09@gmail.com>
2021-02-01 11:46:58 +0530

    adding convert js file

--- routes/convert.js
+++ routes/convert.js
@@ -1,1 +1,13 @@
-export default "convert";
+import express from "express";
+import mathmlToLatex from "mathml-to-latex";
+
+const convertRouter = express.Router();
+
+convertRouter.get("/", (req, res, next) => {
+    const convertString = req.body.data;
+    const result = mathmlToLatex.convert(convertString);
+
+    return res.json({ data: result });
+});
+
+export default convertRouter;
